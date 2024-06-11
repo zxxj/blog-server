166385e286551cc7ba17fd9af1a9a0b10b29ba9a
zhangxinxin <1285367184@qq.com>
2024-06-11 22:24:30 +0800

    feat(登录/注册/文章): 登录注册与文章接口初步实现

---
+++ routes/articles.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+
+// 发布文章
+router.post('/', (req, res, next) => {
+  console.log(req.body);
+
+  res.json({
+    code: 1,
+    message: "文章发布成功!",
+  })
+})
+
+// 根据用户id获取文章列表
+router.get("/users/:userId", (req, res, next) => {
+  console.log(req.params);
+
+  res.json({
+    code: 1,
+    message: "查询成功!"
+  })
+})
+
+// 根据文章id修改文章内容
+router.patch("/:articleId", (req, res, next) => {
+  console.log(req.params);
+  console.log(req.body);
+  res.json({
+    code: 1,
+    message: "文章修改成功!"
+  })
+})
+
+// 删除文章
+router.delete('/:articleId', (req, res, next) => {
+  console.log(req.params);
+  res.json({
+    code: 1,
+    message: "文章删除成功!"
+  })
+})
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+// 注册
+router.post('/', (req, res, next) => {
+  console.log(req.body);
+  res.json({
+    code: 1,
+    message: "注册成功!"
+  })
+})
+
+// 登录
+router.get('/', (req, res, next) => {
+  console.log(req.query);
+  res.json({
+    code: 1,
+    message: "登录成功"
+  })
+})
+
+module.exports = router;
