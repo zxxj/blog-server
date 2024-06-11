1e592b9b13c745cec6cbccd0b648db5f794ad558
zhangxinxin <1285367184@qq.com>
2024-06-11 23:06:10 +0800

    feat(jwt): 生成token与返回token

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken')
 
 // 注册
 router.post('/', (req, res, next) => {
   console.log(req.body);
-  res.json({
-    code: 1,
-    message: "注册成功!"
-  })
+    res.json({
+      code: 1,
+      message: "注册成功!",
+      token
+    })
 })
 
 // 登录
 router.get('/', (req, res, next) => {
   console.log(req.query);
-  res.json({
-    code: 1,
-    message: "登录成功"
-  })
+    if(req.query.username === 'admin' && req.query.password === '123456') {
+    const token = jwt.sign({username: req.query.username}, 'textjwt', {expiresIn: '60s', algorithm: 'HS256'})
+    res.json({
+      code: 1,
+      message: "登录成功!",
+      token
+    })
+  }else {
+    res.json({
+      code: 0,
+      message: "登录失败"
+    })
+  }
 })
 
 module.exports = router;
