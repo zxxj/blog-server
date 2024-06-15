462e5b105d0da395b80c6821c1294114c47aacbd
zhangxinxin <1285367184@qq.com>
2024-06-15 23:51:13 +0800

    feat(Schema与数据模型): 删除单条与多条数据

--- models/index.js
+++ models/index.js
@@ -30,12 +30,59 @@ const ArticleSchema = new Schema({
 // 创建数据模型,根据表结构创建数据模型,并且将表结构映射数据中一个Articles这个表,通过Articles表对象就可以对数据库中的数据进行增删改查操作
 const Article = mongoose.model("Article", ArticleSchema)
 
-// 向表中插入数据
-Article.create({
-	title: "测试文章",
-	content: "测试文章内容",
-	author: "测试作者",
-	tag: "测试文章类型",
-}).then(res => {
-	console.log(res);
-})
+// 向表中插入一条数据
+// Article.create({
+// 	title: "测试文章",
+// 	content: "测试文章内容",
+// 	author: "测试作者",
+// 	tag: "测试文章类型",
+// }).then(res => {
+// 	console.log(res);
+// })
+
+// 向表中插入多条数据
+// Article.create([
+// 	{
+// 		title: "测试文章1",
+// 		content: "测试文章内容",
+// 		author: "测试作者",
+// 		tag: "测试文章类型",
+// 	},
+// 	{
+// 		title: "测试文章2",
+// 		content: "测试文章内容2",
+// 		author: "测试作者2",
+// 		tag: "测试文章类型2",
+// 	},
+// 	{
+// 		title: "测试文章3",
+// 		content: "测试文章内容3",
+// 		author: "测试作者3",
+// 		tag: "测试文章类型3",
+// 	},
+// 	{
+// 		title: "测试文章4",
+// 		content: "测试文章内容4",
+// 		author: "测试作者4",
+// 		tag: "测试文章类型4",
+// 	}
+// ]).then(res => {
+// 	console.log(res);
+// })
+
+// 根据id删除表中的数据
+// Article.deleteOne({ _id: '666b0afa6782785774c0a794' }).then(res => {
+// 	console.log("删除成功");
+// }).catch(err => {
+// 	console.log("删除失败");
+// })
+
+
+// 删除多个
+// Article.deleteMany({ content: /测试文章/ }).then(res => {
+// 	console.log(res);
+// 	console.log("删除成功");
+// }).catch(err => {
+// 	console.log(err);
+// 	console.log("删除失败");
+// })
